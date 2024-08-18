5bb5f5190e4e2200f8aefd2476521fc3546c6134
Jubibani <strawberryloli3@gmail.com>
2024-08-18 18:11:10 +0800

    Added a folder with a git folder

--- protoype FSM/states/standing.gd
+++ protoype FSM/states/standing.gd
@@ -2,7 +2,7 @@ extends PlayerState
 
 func enter(previous_state_path: String, data := {}) -> void:
 	print("Entered Standing state")
-
+	player.animation_player.play("CROUCHING", -1, -player.crouching_animation_speed, true)
 func physics_update(_delta: float) -> void:
 	if Input.is_action_pressed("crouch_or_uncrouch"):
 		toggle_crouch()
